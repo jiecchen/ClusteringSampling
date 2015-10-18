53b61fc479d6f0abbc0c048140b0873982d53ebc
jiecchen <jiecchen@indiana.edu>
2015-10-18 01:16:53 -0400

    updated

--- SuccessiveSampling.py
+++ SuccessiveSampling.py
@@ -1,6 +1,6 @@
 import numpy as np
 import sets
-
+import sys
 
 def dist(x, y):
     return np.linalg.norm(x - y)
@@ -8,15 +8,15 @@ def dist(x, y):
 
 class Assoc:
     """
-    Associate a vector with its nearest neighbor
+    Associate this with other
     """
     def __init__(self, _me, _nearest):
-        self.vector = _me
-        self.nearest = _nearest
+        self.this = _me
+        self.other = _nearest
     def getDist(self):
-        return dist(self.vector, self.nearest)
+        return dist(self.this, self.other)
     def __str__(self):
-        return self.vector.__str__() + '-->' + self.nearest.__str__()
+        return self.this.__str__() + '-->' + self.other.__str__()
 
 def find_nearest(x, S):
     m_dist = 1e10
@@ -34,6 +34,13 @@ def get_data(n=1000, k=20):
     arr = [int(x) for x in arr]
     return arr
 
+def detect_outliers(U, nOutliers, alpha=0.001, beta=0.05, M=20):
+    # alpha << beta
+    N = len(U)
+    while len(U) > beta * N:
+        S = np.random.choice(U, alpha * N)
+        
+        
 def successive_sampling(U, nOutliers, alpha, beta):
     """
     U -- set of points
@@ -46,10 +53,15 @@ def successive_sampling(U, nOutliers, alpha, beta):
     n_iter = 0
     centers = []
     max_dist = 0.
-    while len(U) > 5 * nOutliers / (1. - beta):
+    pre_len_U = 0
+    while len(U) > nOutliers / (1. - beta) / 3:
+        if len(U) == pre_len_U:
+            beta -= 0.1
+        pre_len_U = len(U)
         n_iter += 1
         print '>>>>> n_iter =', n_iter
         print '|U| =', len(U)
+        print 'beta =', beta
         # sample 
         S = np.random.choice(U, alpha)
         # add as centers
@@ -70,18 +82,42 @@ def successive_sampling(U, nOutliers, alpha, beta):
                 config.append(a)
                 
         # remove Ball(S, v) from U
-        U = [a.vector for a in assign if a.getDist() >= v]
+        U = [a.this for a in assign if a.getDist() >= v]
+        
 
     # now we want to remove the false positive
     U = [x for x in U if dist(x, find_nearest(x, S)) > max_dist]
         
     return U, config
 
+def read_from_file(fileName, n_skip = 1):
+    lineN = 0
+    data = []
+    outliers = []
+    with open(fileName, 'r') as fin:
+        for line in fin:
+            lineN += 1
+            if lineN > n_skip:
+                line = line.split(',')
+                data.append(float(line[1]))
+                if int(line[2]) == 1:
+                    outliers.append(float(line[1]))
+
+    return data, outliers
+
 if __name__ == '__main__':
-    U = get_data(10000, 20) # return a list
-    U = [x for x in U]
-    nOutliers = 3
-    U.extend([5000 * i for i in xrange(1, nOutliers + 1)])
-    outliers, config = successive_sampling(U, nOutliers, 5, 0.8)
-    print outliers
-    print np.unique(np.array([a.nearest for a in config]))
+    U, outliers = read_from_file(sys.argv[1])
+    # U = get_data(10000, 20) # return a list
+    nOutliers = len(outliers)
+    print 'median =', np.median(U)
+    
+    # U.extend([5000 * i for i in xrange(1, nOutliers + 1)])
+
+    outliers_est, config = successive_sampling(U, nOutliers, 5, 0.9)
+    outliers = sets.Set(outliers)
+    outliers_est = sets.Set(outliers_est)
+    print 'Ext:', sorted(outliers)
+    print 'Est:', sorted(outliers_est)    
+    print 'diff:', outliers.difference(outliers_est)
+    
+    print 'Centers:', np.unique(np.array([a.other for a in config]))
